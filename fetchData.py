af2011c8176a569200183b577fa60dcce225d97d
akariasmorum <akariasmorum@gmail.com>
2018-07-14 13:18:57 +0500

    Add files via upload
    Теперь компилится, ищет схожих названий

--- fetchData.py
+++ fetchData.py
@@ -1,5 +1,32 @@
 import traceback
 
+def fetchData(data, database, massive):
+    for answer in data:
+        ID = answer.select('*/ID').text
+        name = answer.select('*/Name').text
+        #ищем с1ид в базе
+        с1id = lookForC1ID(database, ID)
+        #если есть такой
+        if len(с1id.data)>0:
+            #то сравниваем с1ид с тем, что в базе
+            #если названия разные
+                if с1id.data[0][6] != name:
+                    #то добавить в массив
+                    massive.append([с1id.data[0][0], с1id.data[0][5], name ])
+        #если такого с1ид нет в базе            
+        else:
+            #сразу добавлям в массив
+            massive.append([ 0, ID, name ])
+
+    
+# поиск с1id в базе. если нет, то len(res.data) = 0
+def lookForC1ID(database, c1id):
+    res = database.execute_query(''' 
+        select * from _s_1c_Nomenklature where C1ID=@c1id
+    ''', c1id = c1id )
+    return res
+    
+
 try:
     #массив, куда накапаливаем строки
     massive = []
@@ -20,40 +47,11 @@ try:
     
     fetchData(data, database, massive)
     
-    for i, row in massive:
-        script_output[str(i)] = "{0} / {1} / {3}".format(row[0], row[1], row[2])
+    for i, row in enumerate(massive):
+        script_output[str(i)] = "{0} / {1} / {2}".format(row[0], row[1], row[2])
         
     
-    def fetchData(data, database, massive):
-        
-        for answer in data:
-            ID = answer.select('*/ID').text
-            name = answer.select('*/Name').text
-            #ищем с1ид в базе
-            с1id = lookForC1ID(database, c1id)
-            #если есть такой
-            if len(c1id.data)>0:
-                #то сравниваем с1ид с тем, что в базе
-                #если названия разные
-                    if c1id.data[0][6] != name:
-                        #то добавить в массив
-                        massive.append([c1id.data[0][0], c1id.data[0][5], name ])
-            #если такого с1ид нет в базе            
-            else:
-                #сразу добавлям в массив
-                massive.append([ 0, ID, name ])
-                
-            
-        
-        
-    # поиск с1id в базе. если нет, то len(res.data) = 0
-    def lookForC1ID(database, c1id):
-        res = database.execute_query(''' 
-            select * from _s_1c_Nomenklature where C1ID=@c1id
-        ''', c1id = c1id )
-        return res
     
-    def fetchData()    
     
 except Exception as ex:
     script_output['message'] = traceback.format_exc()    
