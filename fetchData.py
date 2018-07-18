12febea1a151ae039abfcb314132675d667b8eb0
akariasmorum <akariasmorum@gmail.com>
2018-07-18 18:18:33 +0500

    Add files via upload

--- fetchData.py
+++ fetchData.py
@@ -1,4 +1,6 @@
 import traceback
+import collections
+from System import DateTime
 
 #получение ID
 def getID():
@@ -10,8 +12,8 @@ def getID():
 #сопоставление данных
 def fetchData(data, database, massive, server, user):
     try:
-        i=0
-        for answer in data:
+        #сделаю пока десяток
+        for answer in data[:500]:
             ID = answer.select('*/ID').text
             name = answer.select('*/Name').text
             #ищем с1ид в базе
@@ -22,16 +24,15 @@ def fetchData(data, database, massive, server, user):
                 #если названия разные
                     if str(с1id.data[0][6]).replace(' ', '') != str(name).replace(' ', ''):
                         #то добавить в массив
-                        massive.append([с1id.data[0][0], с1id.data[0][5], name, server, user, 1 ])
-                        #script_output[str(i)] = "{0} vs {1}".format(с1id.data[0][6]),name))
-                        #i=i+1
+                        massive.append([с1id.data[0][0],с1id.data[0][0], server,  user, с1id.data[0][5], name, 1 ])
+                        
             #если такого с1ид нет в базе            
             else:
                 #получаем ID доабвляем сразу в массив, [id ]
                 id_x = getID()
-                massive.append([ id_x, ID, name, server, user, 0 ])
+                massive.append([ id_x,id_x, server, user, ID, name,   0 ])
     except Exception as ex:
-        script_output['message'] = traceback.format_exc()    
+        script_output['message'] =  "fetchdata error" + traceback.format_exc()    
     
 # поиск с1id в базе. если нет, то len(res.data) = 0
 def lookForC1ID(database, c1id):
@@ -41,8 +42,9 @@ def lookForC1ID(database, c1id):
     return res
     
     
-def getDataMassive():
+def getDataMassive(namebook):
     try:
+        
         #массив, куда накапаливаем строки
         massive = []
         
@@ -50,7 +52,7 @@ def getDataMassive():
         remote = misbus.get_external_remote(7)
         operation = remote.get_operation("GetRefBook")
         #Формирование пакета
-        namebook = script_input['bookname']
+        
         op_input = operation.build_input_envelope({
             'name': "GetRefBook",
             'children': [
@@ -77,9 +79,134 @@ def getDataMassive():
         return massive
         
     except Exception as ex:
-        script_output['message'] = traceback.format_exc()  
+        script_output['message'] = "get data massive error" + traceback.format_exc()  
         
   
+#часть 2    
+
+
+def getTableStructure(database, table):
+    try:
+        data = database.execute_query('''SELECT COLUMN_NAME
+            FROM vvk_mias.INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME = N'{0}' '''.format(table))
+        stri=""
+        for i, row in enumerate(data.data):
+            if i!=4: #убираем datetime
+                if i!=0:
+                    stri+=","
+                stri+=row[0]    
+        script_output['structure'] = stri
+        return str
+        
+    except Exception as ex:
+        script_output['message'] = "getTableStructure error" + traceback.format_exc()  
+
+
+def selectFromTable(database, tableName, ID):
+    try:
+        query = 'select * from {0} where id = @id'.format(tableName)
+        varss = database.execute_query(query, id=ID)
+        return varss
+    except Exception as ex:
+        script_output['select-Error'] = traceback.format_exc()
+        script_output['select - tableName'] = tableName
+        script_output['select - id'] = '/'.join(str(ord(c)) for c in str(ID))
+    
+#select insert delete    
+def sid(database, tableName, ID):
+    try:
+        #берем строку по ID
+        values = selectFromTable(database, tableName, ID)
+        script_output['sid - values'] = "{0} / {1} / {2} / {3} / {4} / {5} / {6} ".format(values.data[0][0],values.data[0][1],values.data[0][2],values.data[0][3],values.data[0][4],values.data[0][5],values.data[0][6])
+        #Берем структуру таблицы
+        newvalues=[]
+        q=0
+        for i in values.data[0]:
+            if i!=4:
+                newvalues[q] = values.data[0][i]
+                q+=1
+        columnNames = getTableStructure(database, tableName)
+        script_output['sid - columns'] = columNames
+        #вставляем строку в резервную таблицу _d
+        insertDataInTable(database, tableName+'_d', columNames, newvalues)
+        #удаляем ту строку из таблицы
+        deleteDataFromTable(database, tableName, ID)
+    except Exception as ex:
+        script_output['sid-Error'] = traceback.format_exc()
+        script_output['sid - ID'] = ID
+        script_output['sid - tableName'] = tableName
+        
+#вставить это элемент в *_d
+
+def insertSelectByID(database, tableNameInto, tableNameFrom, columNames, ID):
+    
+    columns = "".join((k) for k in columNames)
+    
+    
+    query = '''insert into {0} ({1})
+                select {2}
+                from {3} 
+                where C1ID='e98f782f-5776-11e5-8a23-0015172adb1d' '''.format(tableNameInto, columns, columNames)
+                
+#убрать повторяющийся код
+def insertDataInTable(database, values):
+    query = ''' insert into _s_1c_Nomenklature (ID, ID1, ID_SERVER, ID_LOGIN, C1ID, NAME)
+                VALUES (@a, @b, @c, @d, @e, @f )'''
+    
+    data = database.execute_nonquery(query, 
+        a = values[0],
+        b = values[1],
+        c = values[2],
+        d = values[3],
+        e = values[4],
+        f = values[5])
+    
+    return data
+    
+def insertDataInTableD(database, values):
+    
+    query = ''' insert into _s_1c_Nomenklature_d (ID, ID1, ID_SERVER, ID_LOGIN, DATETIME_C,  C1ID, NAME)
+                VALUES (@a, @b, @c, @d, @e, @f,  @g )'''
+    try:
+        data = database.execute_nonquery(query, 
+            a = values[0],
+            b = values[1],
+            c = values[2],
+            d = values[3],
+            e = values[4],
+            f = values[5],
+            g = values[6])
+        
+        return data
+    except Exception as ex:
+        script_output['query'] = query
+        
+
+#удалить элемент из таблицы
+def deleteDataFromTable(database, tableName, ID):
+    query = ''' DELETE FROM {0} WHERE ID=@id '''.format(tableName)
+    num = database.execute_nonquery(query, table=tableName, id=ID)  
+    return num
+
+
+bookname = script_input['bookname']    
+massive = getDataMassive(bookname)
+db = misbus.get_internal_remote('test-sql')
+tableName = '_s_1c_Nomenklature'
+massive.insert(4, DateTime.Now)
+for i,x in enumerate(massive):
+    insertDataInTableD(db, x)
+script_output['result'] = 'OK'    
+    
+
+
+    
+    
+
+    
+
+
         
 
 
